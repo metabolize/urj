39cee21f0f3a0c5a9c73570390c5d5450e26cff2
Paul Melnikow <email@paulmelnikow.com>
2018-11-27 14:50:04 -0500

    Load s3Uri from npm

--- src/common-contracts.js
+++ src/common-contracts.js
@@ -1,40 +1,10 @@
 'use strict'
 
 const c = require('rho-contracts-fork'),
-  s3BucketName = require('rho-cc-s3-bucket-name'),
-  url = require('url'),
-  _ = require('underscore')
+  s3Uri = require('rho-cc-s3-uri')
 
 const cc = (module.exports = {})
 
 cc.callback = require('rho-cc-node-style-callback').withDefaultError(c.error)
 
-cc.s3Uri = c
-  .pred(function(value) {
-    if (!_(value).isString()) {
-      return false
-    }
-
-    const parsed = url.parse(value)
-
-    const disallowedKeys = ['auth', 'port', 'search', 'query', 'hash']
-    const isPresent = function(key) {
-      return parsed[key] !== null
-    }
-    if (_(disallowedKeys).any(isPresent)) {
-      return false
-    }
-
-    if (parsed.protocol !== 's3:') {
-      return false
-    }
-
-    try {
-      s3BucketName.check(parsed.host)
-    } catch (e) {
-      return false
-    }
-
-    return true
-  })
-  .rename('s3Uri')
+cc.s3Uri = s3Uri
